d0889665292c0b58fbee576d1f07045af71aeb77
Azqia Kayla M. A <112361939+kaylapplg@users.noreply.github.com>
2023-08-01 21:45:08 -0700

    Add files via upload

---
+++ pengaduan_masyarakat/daftar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><b><center>REGISTRASI</center></b></h1>
+    <center>
+    <div>
+    <img src="login.png" 
+    style="display:block; margin:auto; widht:250px; height:250px;">
+    </div>
+    <form action= "tampilanlogin.php" method="post">
+        <p>Nik: <input type="text" name="nik" id=""></p>
+        <p>Nama: <input type="text" name="nama" id =""></p>
+        <p>Username: <input type="text" name="username" id =""></p>
+        <p>Password: <input type="text" name="password" id =""></p>
+        <p>Telepon: <input type="text" name="telepon" id =""></p>
+        <button type="submit">Daftar</button>
+    </form>
+    </center>
