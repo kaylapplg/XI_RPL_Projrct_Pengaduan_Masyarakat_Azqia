e1f0ae6b24fb3b401cc5b8b1fad5b5e250c89499
Azqia Kayla M. A <112361939+kaylapplg@users.noreply.github.com>
2023-08-08 21:40:29 -0700

    daftar.php

--- pengaduan_masyarakat/daftar.php
+++ pengaduan_masyarakat/daftar.php
@@ -2,23 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+<div style="border: 0; padding: 3px; background-color: #1C2870; text-align: left;">
+</div>
+<div style="border: 0; padding: 20px; background-color: white; text-align: left;"></div>
 <body>
-    <h1><b><center>REGISTRASI</center></b></h1>
-    <center>
+<center><h2><font size="6"><font color="#1C2870"><font face="comic sans ms">REGISTRASI</font></font></font></h2></center>
     <div>
-    <img src="login.png" 
+    <img src="registrasi.png.png" 
     style="display:block; margin:auto; widht:250px; height:250px;">
     </div>
+    <center>
     <form action= "tampilanlogin.php" method="post">
-        <p>Nik: <input type="text" name="nik" id=""></p>
-        <p>Nama: <input type="text" name="nama" id =""></p>
-        <p>Username: <input type="text" name="username" id =""></p>
-        <p>Password: <input type="text" name="password" id =""></p>
-        <p>Telepon: <input type="text" name="telepon" id =""></p>
-        <button type="submit">Daftar</button>
+        <p><font face="comic sans ms">nik: </font><input type="text" name="nik" id=""></p>
+        <p><font face="comic sans ms">nama: </font><input type="text" name="nama" id =""></p>
+        <p><font face="comic sans ms">username: </font><input type="text" name="username" id =""></p>
+        <p><font face="comic sans ms">email: </font><input type="text" name="email" id =""></p>
+        <p><font face="comic sans ms">password: </font><input type="text" name="password" id =""></p>
+        <p><font face="comic sans ms">telepon: </font><input type="text" name="telepon" id =""></p>
+        <button style="background-color:#1C2870;" type="submit"><font color="white"><font face="comic sans ms">Daftar</font></font></button>
     </form>
+    <div style="border: 0; padding: 28px; background-color: white; text-align: left;"></div>
+    <div style="border: 0; padding: 3px; background-color: #1C2870; text-align: left;"></div>
     </center>
+</body>
+</html>
