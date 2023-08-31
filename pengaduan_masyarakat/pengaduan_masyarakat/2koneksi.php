86359b4f47915f20d01f2c65eb9a30e2a8ce1000
Azqia Kayla M. A <112361939+kaylapplg@users.noreply.github.com>
2023-08-30 22:23:17 -0700

    Delete pengaduan_masyarakat directory

--- pengaduan_masyarakat/pengaduan_masyarakat/2koneksi.php
+++
@@ -1,3 +0,0 @@
-<?php
-$koneksi = new PDO("mysql:host=localhost;dbname=db_pengaduan_masyarakat_kayla","root","");
-?>
--- pengaduan_masyarakat/pengaduan_masyarakat/proses_hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-$id = $_GET['id'];
-
-$koneksi = new PDO('mysql:host=localhost;dbname=db_pengaduan_masyarakat_kayla', 'root', '');
-
-
-$koneksi->query("DELETE FROM pengaduan where id_pengaduan='$id'");
-
-header("location:../home.php");                                                                                                                          
